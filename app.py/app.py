f634b855756cee43f1669f9c4bdc62109a593e1a
Elza Hayyat <ekhayat@g.ucla.edu>
2022-03-17 23:15:57 -0700

    new files added

---
+++ app.py/app.py
@@ -0,0 +1,10 @@
+#Import Flask
+from flask import Flask, jsonify
+
+#Create an app
+app = Flask (_name_)
+
+#Define what to do when a user hits the index route
+@app.route ("/")
+def home():
+    print ()
